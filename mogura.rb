8b840913de0bbfb284dd99f61b9181f20ab8ef5a
reiki4040 <reiki4040@gmail.com>
2019-03-04 23:34:15 +0900

    fixed sha256 variable name.

--- mogura.rb
+++ mogura.rb
@@ -3,7 +3,7 @@ class Mogura < Formula
   homepage "https://github.com/reiki4040/mogura"
   version "0.1.0"
   url "https://github.com/reiki4040/mogura/releases/download/#{version}/mogura-#{version}-darwin-amd64.zip"
-  normal_sha256 = "6f2781ab73f244b666871c4ebe18811952dda7e3c5f8fbdf6a4cf7d735da3284"
+  sha256 = "6f2781ab73f244b666871c4ebe18811952dda7e3c5f8fbdf6a4cf7d735da3284"
 
   def install
     bin.install "mogura"
