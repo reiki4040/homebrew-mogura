ad672825e86f6e096f112eede0d7816e4d9ef623
reiki4040 <reiki4040@gmail.com>
2019-07-20 17:45:49 +0900

    mogura 0.1.4

--- mogura.rb
+++ mogura.rb
@@ -1,9 +1,9 @@
 class Mogura < Formula
   desc "ssh tunneling tool"
   homepage "https://github.com/reiki4040/mogura"
-  version "0.1.3"
+  version "0.1.4"
   url "https://github.com/reiki4040/mogura/releases/download/#{version}/mogura-#{version}-darwin-amd64.zip"
-  sha256 = "61bb8880fde05df001a985adfce4ad9c1dcd7ef4ed54602e5d305b8d6947221d"
+  sha256 = "6b24d4098214e3878c67f8d25a597f8e26b5f89091a0954a5a6f971225dd23f1"
 
   def install
     bin.install "mogura"
